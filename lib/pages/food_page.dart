bf436bdf181c045f243a2bf72e23b5c21d639204
thuan <thuanvo.140702@gmail.com>
2024-07-17 15:11:18 +0700

    food_page.dart

--- lib/pages/food_page.dart
+++ lib/pages/food_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_button.dart';
 import 'package:food_delivery_app/models/food.dart';
+
 class FoodPage extends StatefulWidget {
   final Food food;
-  final Map<Addon,bool> selection= {};
-  FoodPage({super.key, required this.food}){
-    for (Addon addon in food.availableAddons){
+  final Map<Addon, bool> selection = {};
+  FoodPage({super.key, required this.food}) {
+    for (Addon addon in food.availableAddons) {
       selection[addon] = false;
     }
   }
@@ -17,99 +18,120 @@ class FoodPage extends StatefulWidget {
 class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar() ,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            //food image
-            Image.asset(widget.food.imagePath),
+    return Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                //food image
+                Image.asset(widget.food.imagePath),
 
-            // food name
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.food.name,
-                    style:
-                      TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
+                // food name
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.food.name,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
                       ),
-                  ),
 
-                  Text(
-                    '\$ '+ widget.food.price.toString(),
-                    style:
-                    TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Theme.of(context).colorScheme.primary
+                      Text(
+                        '\$ ' + widget.food.price.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
 
-                    ),
-                  ),
+                      // food description
+                      Text(
+                        widget.food.description,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Theme.of(context).colorScheme.primary),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
 
-                  // food description
-                  Text(
-                    widget.food.description,
-                    style:
-                    TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25,
-                      color: Theme.of(context).colorScheme.primary
-
-                    ),
-                  ),
-                  const SizedBox(height: 10,),
-
-                  Divider(color: Theme.of(context).colorScheme.secondary,),
-
-
-                  Text("Add-ons",style: TextStyle(fontSize: 20,color: Theme.of(context).colorScheme.inversePrimary),),
-
-                  // addons
-                  Container(
-                    decoration: BoxDecoration(
-
-                      border: Border.all(
+                      Divider(
                         color: Theme.of(context).colorScheme.secondary,
                       ),
-                      borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      padding: EdgeInsets.zero,
-                      itemCount: widget.food.availableAddons.length,
-                      itemBuilder: (context,indedx){
-                        //get individual addons
-                        Addon addon = widget.food.availableAddons[indedx];
-                        return CheckboxListTile(
-                          title: Text(addon.name),
-                          subtitle: Text('\$ '+addon.prices.toString()),
-                          value: widget.selection[addon],
-                          onChanged: (bool? value){
-                            setState(() {
-                              widget.selection[addon] = value!;
-                            });
-                          },
-                        );
-                      }),
+
+                      Text(
+                        "Add-ons",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary),
+                      ),
+
+                      // addons
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Theme.of(context).colorScheme.secondary,
+                            ),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            padding: EdgeInsets.zero,
+                            itemCount: widget.food.availableAddons.length,
+                            itemBuilder: (context, indedx) {
+                              //get individual addons
+                              Addon addon = widget.food.availableAddons[indedx];
+                              return CheckboxListTile(
+                                title: Text(addon.name),
+                                subtitle: Text('\$ ' + addon.prices.toString()),
+                                value: widget.selection[addon],
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    widget.selection[addon] = value!;
+                                  });
+                                },
+                              );
+                            }),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                // button add to card
+                MyButton(onTap: () {}, text: "Add-to-card"),
+                SizedBox(
+                  height: 20,
+                )
+              ],
             ),
-            // button add to card
-            MyButton(
-              onTap: (){},
-              text: "Add-to-card"
-            ),
-            SizedBox(height: 20,)
-          ],
+          ),
         ),
-      ),
+
+        // back button
+        SafeArea(
+          child: Opacity(
+            opacity: 0.7,
+            child: Container(
+              margin: const EdgeInsets.only(left: 25),
+              decoration:
+                  BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      shape: BoxShape.circle
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back_ios_rounded),
+                    onPressed: ()=>Navigator.pop(context),
+                  ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
